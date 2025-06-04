bc030b7a8ec74351a57ccb7c69394e448150fb22
llyu-v <linshuoshuo.lyu@vumc.org>
2025-06-04 12:19:03 -0500

    Update ldsc_munge_sums_EA.sh

--- GWAS_Meta/LDSC/ldsc_munge_sums_EA.sh
+++ GWAS_Meta/LDSC/ldsc_munge_sums_EA.sh
@@ -13,34 +13,33 @@ source activate ldsc
 
 # For overall IBD:
 /home/lyul1/ldsc/munge_sumstats.py \
---sumstats /nobackup/sbcs/lyul1/IBD/metal_update/withUKB/gc_0.01/GC_Final/processed/EASEUR_CD_META1.TBL.SNP2.txt \
+--sumstats /nobackup/sbcs/keep/data/GuoLab/backup/lyul1/IBD/metal_update/withUKB/gc_0.01/GC_Final/processed/EASEUR_CD_META1.TBL.SNP2.txt \
 --snp SNP \
 --N 27465 \
 --chunksize 500000 \
 --a1 Allele2 \
 --a2 Allele1 \
---out eas_cd \
+--out /nobackup/sbcs/keep/data/GuoLab/backup/lyul1/IBD/single_cell/sLDSCs/sumstats/eas_cd \
 --merge-alleles /home/lyul1/LDSCref/w_hm3.snplist
 
 /home/lyul1/ldsc/munge_sumstats.py \
---sumstats /nobackup/sbcs/lyul1/IBD/metal_update/withUKB/gc_0.01/GC_Final/processed/EASEUR_UC_META1.TBL.SNP2.txt \
+--sumstats /nobackup/sbcs/keep/data/GuoLab/backup/lyul1/IBD/metal_update/withUKB/gc_0.01/GC_Final/processed/EASEUR_UC_META1.TBL.SNP2.txt \
 --snp SNP \
 --N 35347 \
 --chunksize 500000 \
 --a1 Allele2 \
 --a2 Allele1 \
---out eas_uc \
+--out /nobackup/sbcs/keep/data/GuoLab/backup/lyul1/IBD/single_cell/sLDSCs/sumstats/eas_uc \
 --merge-alleles /home/lyul1/LDSCref/w_hm3.snplist
 
 
 
 /home/lyul1/ldsc/munge_sumstats.py \
---sumstats /nobackup/sbcs/lyul1/IBD/metal_update/withUKB/gc_0.01/GC_Final/processed/EASEUR_IBD_META1.TBL.SNP2.txt \
+--sumstats /nobackup/sbcs/keep/data/GuoLab/backup/lyul1/IBD/metal_update/withUKB/gc_0.01/GC_Final/processed/EASEUR_IBD_META1.TBL.SNP2.txt \
 --snp SNP \
 --N 63415 \
 --chunksize 500000 \
 --a1 Allele2 \
 --a2 Allele1 \
---out eas_ibd \
+--out /nobackup/sbcs/keep/data/GuoLab/backup/lyul1/IBD/single_cell/sLDSCs/sumstats/eas_ibd \
 --merge-alleles /home/lyul1/LDSCref/w_hm3.snplist
-
